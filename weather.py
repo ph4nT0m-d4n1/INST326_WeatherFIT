b22e939b2e4f79ee4ea1cc0f3a02c1e99dec541d
ph4nT0m <147522954+ph4nT0m-d4n1@users.noreply.github.com>
2025-04-20 15:40:58 -0400

    added weather api and simple function

--- weather.py
+++ weather.py
@@ -1,49 +1,100 @@
-"""Weather App (Check in 1)
-Danny Mallya
-Bella Konrad
-Jaden Shin
-Marvin Gomez Molina
+"""WeatherFit, an INST326 project
+Group Members:
+    Danny Mallya
+    Bella Konrad
+    Jaden Shin
+    Marvin Gomez Molina
 
 Professor Cruz
 4/20/25
 
 This program reads data from a weather api and determines the upcoming forecast. 
 The app will then suggest clothing that would be appropriate for the weather condtions.
-""""
+"""
+import openmeteo_requests
 
-class Forecast:
-    """This class 
+import requests_cache
+import pandas as pd
+from retry_requests import retry
+
+class Forecast():
+    """This class is used to get the weather forecast for a given location and date.
+        Attributes:
+            date (str): the date for the forecast
+            temperature (float): the temperature in fahrenheit for the day
+            humidity (float): the humidity percentage for the day
+            precipitation_chance (float): chance of precipitation for the day
+            precipitation_type (str): type of precipitation for the day (if any)
+            cloud_coverage (float): percentage of cloud coverage for the day
+            wind_speed (float): The wind speed in miles per hour for the day
     """
     
-    def __init__(self, date, temperature, humidity, precipitation_chance, precipitation_type, cloud_coverage, wind_speed):
+    def __init__(self, date=None, temperature=None, humidity=None, precipitation_chance=None, cloud_coverage=None, wind_speed=None):
         """This function initializes key attributes
 
             Attributes:
-                date: the day
-                temperature: temperature in fahrenheit for the day
-                humidity: humidity percentage for the day
-                precipitation_chance: chance of precipitation for the day
-                precipitation_type: type of precipitation for the day (if any)
-                cloud_coverage: percentage of cloud coverage for the day
-                wind_speed: The wind speed in miles per hour for the day
+                date (str): the date for the forecast
+                temperature (float): the temperature in fahrenheit for the day
+                humidity (float): the humidity percentage for the day
+                precipitation_chance (float): chance of precipitation for the day
+                precipitation_type (str): type of precipitation for the day (if any)
+                cloud_coverage (float): percentage of cloud coverage for the day
+                wind_speed (float): The wind speed in miles per hour for the day
         """
         self.date = date
         self.temperature = temperature
         self.humidity = humidity
         self.precipitation_chance = precipitation_chance
-        self.precipitation_type = precipitation_type
         self.cloud_coverage = cloud_coverage
         self.wind_speed = wind_speed
     
-    def get_data(self, weather_api):
+    def get_current_forecast(self, latitude, longitude):
         """This function gets the data from the api
 
             Attributes:
-                weather_api: the
+                latitude (float): the latitude of the location
+                longitude (float): the longitude of the location
         """
-        pass
-    
-    
+        # setup the Open-Meteo API client with cache and retry on error
+        cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+        retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+        openmeteo = openmeteo_requests.Client(session = retry_session)
+        
+        # the order of variables in hourly or daily is important to assign them correctly
+        url = "https://api.open-meteo.com/v1/forecast"
+        params = {
+            "latitude": latitude,
+            "longitude": longitude,
+            "models": "best_match",
+            "current": ["temperature_2m", "relative_humidity_2m", "precipitation", "wind_speed_10m", "cloud_cover"],
+            "timezone": "auto",
+            "forecast_days": 1,
+            "wind_speed_unit": "mph",
+            "temperature_unit": "fahrenheit",
+            "precipitation_unit": "inch"
+        }
+        responses = openmeteo.weather_api(url, params=params)
+        
+        response = responses[0]
+        print(f"Coordinates: {response.Latitude()}°N {response.Longitude()}°E")
+        print(f"Timezone: {response.Timezone()}{response.TimezoneAbbreviation()}\n")
+        
+        current = response.Current()
+        
+        self.temperature = current.Variables(0).Value()
+        self.temperature = current.Variables(0).Value()
+        self.humidity = current.Variables(1).Value()
+        self.precipitation_chance = current.Variables(2).Value()
+        self.wind_speed = current.Variables(3).Value()
+        self.cloud_coverage = current.Variables(4).Value()
+        
     
     def __repr__(self):
-        return f"The forecast for {self.date}:\nTemperature: {self.temperature}\nHumidity: {self.humidity}\nPrecipitation: {self.precipitation}\nCloud Coverage: {self.cloud_coverage}"
+        return f"The current forecast: \
+                \nTemperature: {self.temperature}\nHumidity: {self.humidity}\nPrecipitation: {self.precipitation_chance}\
+                \nWind Speed: {self.wind_speed}\nCloud Coverage: {self.cloud_coverage}"
+
+if __name__ == "__main__":
+    weather = Forecast()
+    weather.get_current_forecast(38.9807, -76.9369) # Example coordinates for College Park, MD
+    print(weather)
